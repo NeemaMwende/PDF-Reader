357595c3fada9e7e936f2eb2cb2cf5dd4b8bff2a
Neema <neemamwende009@gmail.com>
2024-10-16 14:47:15 +0300

    updates

--- pdfreader/pdfreaderapp/views.py
+++ pdfreader/pdfreaderapp/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+from django.http import JsonResponse
+from .models import PDFDocument
+from PyDF2 import PdfReader
+
+def upload_pdf(request):
+    if request.method == 'POST':
+        pdf = request.FILES['file']
+        document = PDFDocument.objects.create(file=pdf)
+        
+        reader = PdfReader(document.file)
+        full_text = ""
+        for page in  reader.pages:
+            full_text += page.extract_text()
+            
+        return  JsonResponse({'message': "File uploaded  successfully", 'text': full_text})
+    return JsonResponse({"error": "Invalid request"})
