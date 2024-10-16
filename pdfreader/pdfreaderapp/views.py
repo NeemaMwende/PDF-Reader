a166fe26fc8ac048ce6519092bc800c4313e2e46
Neema <neemamwende009@gmail.com>
2024-10-16 15:02:55 +0300

    updates

--- pdfreader/pdfreaderapp/views.py
+++ pdfreader/pdfreaderapp/views.py
@@ -5,6 +5,9 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from .models import PDFDocument
 from PyDF2 import PdfReader
+from transformers import pipeline
+
+qa_pipeline = pipeline("question-answering")
 
 def upload_pdf(request):
     if request.method == 'POST':
@@ -18,3 +21,13 @@ def upload_pdf(request):
             
         return  JsonResponse({'message': "File uploaded  successfully", 'text': full_text})
     return JsonResponse({"error": "Invalid request"})
+
+def answer_question(request):
+    question = request.POST.get('question')
+    document_text = request.POST.get('document_text')
+    
+    response = qa_pipeline({
+        'question': question,
+        'context': document_text
+    })
+    return JsonResponse({'answer': response['answer']})
